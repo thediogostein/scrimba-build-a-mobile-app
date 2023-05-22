722d3cb216f0ec63e316967f572340d9facfc6cb
bruxo <diogo.stein@outlook.com.br>
2023-05-22 14:36:37 -0300

    Style list items

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ import {
   getDatabase,
   ref,
   push,
+  onValue,
 } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js';
 
 const appSettings = {
@@ -16,13 +17,34 @@ const shoppingListInDB = ref(database, 'shoppingList');
 
 const inputFieldEl = document.getElementById('input-field');
 const addButtonEl = document.getElementById('add-button');
+const shoppingListEl = document.getElementById('shopping-list');
 
 addButtonEl.addEventListener('click', function () {
   let inputValue = inputFieldEl.value;
 
   push(shoppingListInDB, inputValue);
-  const shoppingList = document.querySelector('#shopping-list');
-  const li = document.createElement('li');
-  li.innerText = inputValue;
-  shoppingList.append(li);
+
+  clearInputFieldEl();
 });
+
+onValue(shoppingListInDB, function (snapshot) {
+  let itemsArray = Object.values(snapshot.val());
+
+  clearShoppingListEl();
+
+  for (let i = 0; i < itemsArray.length; i++) {
+    appendItemToShoppingListEl(itemsArray[i]);
+  }
+});
+
+function clearShoppingListEl() {
+  shoppingListEl.innerHTML = '';
+}
+
+function clearInputFieldEl() {
+  inputFieldEl.value = '';
+}
+
+function appendItemToShoppingListEl(itemValue) {
+  shoppingListEl.innerHTML += `<li>${itemValue}</li>`;
+}
